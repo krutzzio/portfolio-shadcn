22a74a21ed0ca86ec5c0b6746dbcfb0d8b504c75
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2024-06-01 18:01:50 +0200

    added netlify to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), icon()]
+  }), icon()],
+  output: "server",
+  adapter: netlify()
 });
