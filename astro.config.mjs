abee9065c213d84096dcfe67bd8c52769d2a2f4d
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2024-06-01 18:24:55 +0200

    astro changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
+  integrations: [icon(), react(), tailwind({
     applyBaseStyles: false
-  }), icon()],
-  output: "server",
+  })],
+  output: "static",
   adapter: netlify()
 });
