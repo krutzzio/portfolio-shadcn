141ddc93c7805fd124b456022ab8212cecff097c
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2024-06-01 18:13:52 +0200

    test

--- src/components/islands/NavbarMenu.tsx
+++ src/components/islands/NavbarMenu.tsx
@@ -6,8 +6,6 @@ import {
     DropdownMenuContent,
     DropdownMenuGroup,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
